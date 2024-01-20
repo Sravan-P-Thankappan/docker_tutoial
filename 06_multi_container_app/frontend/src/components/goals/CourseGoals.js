40a24d640679a62c77fb7ff579ef2e71effbccd2
sravan <sravanpthankappan@gmail.com>
2024-01-20 15:33:43 +0530

    multi container communication completed

---
+++ 06_multi_container_app/frontend/src/components/goals/CourseGoals.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './CourseGoals.css';
+import Card from '../UI/Card';
+import GoalItem from './GoalItem';
+
+function CourseGoals(props) {
+  console.log('goals')
+  const hasNoGoals = !props.goals || props.goals.length === 0;
+
+  return (
+    <section id='course-goals'>
+      <Card>
+        {!hasNoGoals && <h1>Your Goals</h1>}
+        {hasNoGoals && <h2>No goals found. Start adding some!</h2>}
+        <ul>
+          {props.goals.map((goal) => (
+            <GoalItem
+              key={goal.id}
+              id={goal.id}
+              text={goal.text}
+              onDelete={props.onDeleteGoal}
+            />
+          ))}
+        </ul>
+      </Card>
+    </section>
+  );
+}
+
+export default CourseGoals;
--- README.md
+++ README.md
@@ -14,11 +14,16 @@
 
 > Containerize a node.js app
 
-**Module 2:**Managing Data and Working with Volumes and Bind Mounts
+**Module 3:**Managing Data and Working with Volumes and Bind Mounts
 
 > Data prerist using volume
 
 > Mount the source code into container using bind mounts
+
+**Module 3:**Cross container communication
+
+> Containarized 3 application react app,node app and mongodb and communicated each other through network
+
 
 
 
